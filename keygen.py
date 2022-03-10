6202789a44aedababeeb48c1cc8c6ed89c6d68d9
C0okiesnCream <69062323+C0okiesnCream@users.noreply.github.com>
2022-03-09 20:58:17 -0700

    Created the key generator
    still needs a few characters but its working

---
+++ keygen.py
@@ -0,0 +1,44 @@
+#imports
+import random
+
+#vars
+dummy = ""
+key = 0
+keyBase = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    'A',
+    'B',
+    'C',
+]
+
+#shuffling base
+random.shuffle(keyBase)
+
+#turning key into a string for easier input
+key = (dummy.join(keyBase))
+print(key)
