073e83327b3eaa5004074d9543b344daae47bcfc
C0okiesnCream <69062323+C0okiesnCream@users.noreply.github.com>
2022-03-11 13:21:29 -0700

    decrypt and encrypt mostly done
    still some bugs that need fixed

--- encrypt.py
+++ encrypt.py
@@ -6,8 +6,13 @@
 #vars
 c = 0
 dummy = []
+#key_file open
 key_file = open("key_file.txt", "r")
+#grabs key from key_file
 key = key_file.readline(85)
+#close key_file
+key_file.close()
+
 message = 0
 encMessage = []
 KEYBASE = [
@@ -112,7 +117,7 @@ for i in message:
  dummy.append(i)
 message = dummy
 
-#it's 1:00 rn so ill continue this tomorrow, if you can see the pattern here go ahead and continue, only gotta do it 40 more times
+#this scrambles the message
 while c < len(message):
  if message[c] == KEYBASE[0]:
   message[c] = key[0]
@@ -202,9 +207,87 @@ while c < len(message):
   message[c] = key[43]
  elif message[c] == KEYBASE[44]:
   message[c] = key[44]
+ elif message[c] == KEYBASE[45]:
+  message[c] = key[45]
+ elif message[c] == KEYBASE[46]:
+  message[c] = key[46]
+ elif message[c] == KEYBASE[47]:
+  message[c] = key[47]
+ elif message[c] == KEYBASE[48]:
+  message[c] = key[48]
+ elif message[c] == KEYBASE[49]:
+  message[c] = key[49]
+ elif message[c] == KEYBASE[50]:
+  message[c] = key[50]
+ elif message[c] == KEYBASE[51]:
+  message[c] = key[51]
+ elif message[c] == KEYBASE[52]:
+  message[c] = key[52]
+ elif message[c] == KEYBASE[53]:
+  message[c] = key[53]
+ elif message[c] == KEYBASE[54]:
+  message[c] = key[54]
+ elif message[c] == KEYBASE[55]:
+  message[c] = key[55]
+ elif message[c] == KEYBASE[56]:
+  message[c] = key[56]
+ elif message[c] == KEYBASE[57]:
+  message[c] = key[57]
+ elif message[c] == KEYBASE[58]:
+  message[c] = key[58]
+ elif message[c] == KEYBASE[59]:
+  message[c] = key[59]
+ elif message[c] == KEYBASE[60]:
+  message[c] = key[60]
+ elif message[c] == KEYBASE[61]:
+  message[c] = key[61]
+ elif message[c] == KEYBASE[62]:
+  message[c] = key[62]
+ elif message[c] == KEYBASE[63]:
+  message[c] = key[63]
+ elif message[c] == KEYBASE[64]:
+  message[c] = key[64]
+ elif message[c] == KEYBASE[65]:
+  message[c] = key[65]
+ elif message[c] == KEYBASE[66]:
+  message[c] = key[66]
+ elif message[c] == KEYBASE[67]:
+  message[c] = key[67]
+ elif message[c] == KEYBASE[68]:
+  message[c] = key[68]
+ elif message[c] == KEYBASE[69]:
+  message[c] = key[69]
+ elif message[c] == KEYBASE[70]:
+  message[c] = key[70]
+ elif message[c] == KEYBASE[71]:
+  message[c] = key[71]
+ elif message[c] == KEYBASE[72]:
+  message[c] = key[72]
+ elif message[c] == KEYBASE[73]:
+  message[c] = key[73]
+ elif message[c] == KEYBASE[74]:
+  message[c] = key[74]
+ elif message[c] == KEYBASE[75]:
+  message[c] = key[75]
+ elif message[c] == KEYBASE[76]:
+  message[c] = key[76]
+ elif message[c] == KEYBASE[77]:
+  message[c] = key[77]
+ elif message[c] == KEYBASE[78]:
+  message[c] = key[78]
+ elif message[c] == KEYBASE[79]:
+  message[c] = key[79]
+ elif message[c] == KEYBASE[80]:
+  message[c] = key[80]
+ elif message[c] == KEYBASE[81]:
+  message[c] = key[81]
+ elif message[c] == KEYBASE[82]:
+  message[c] = key[82]
+ elif message[c] == KEYBASE[83]:
+  message[c] = key[83]
 
  c += 1
 
-dummy = " "
+dummy = ""
 message = dummy.join(message)
-print(message)
+print('message >>>' + message)
